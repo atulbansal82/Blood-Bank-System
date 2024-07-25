239d78c3808b976736f8ea2a4aad464e86b04dcd
Atul Bansal <96112121+atulbansal82@users.noreply.github.com>
2024-07-25 15:10:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
+require("dotenv").config({path: '../.env'});
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 
